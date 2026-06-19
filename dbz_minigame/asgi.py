5833a08c4a1b9a3085990f71e7985b6571f4b7e5
Rustic Developer <rivas.daniel325@gmail.com>
2026-06-19 11:26:06 -0500

    django project, ci config and general config created 19/06/2026

---
+++ dbz_minigame/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dbz_minigame project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dbz_minigame.settings')
+
+application = get_asgi_application()
